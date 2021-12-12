4a2164dec3c8e5d1bdb43d1fdd9835f3ba592d68
Nicolas Morice <nicolas@toogonet.com>
2021-12-12 15:17:47 +0000

    fix #2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,9 +12,9 @@ public class Greeter {
 
   }
 
-  /** 
+  /**
    * @param someone the name of a person
-   * return greeting String
+   * @return greeting String
    */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
